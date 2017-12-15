a1e86644ee925bd4c9f176176c3af28e779a0fdd
muhalifalgibran <alifalgibran147@gmail.com>
2017-12-15 17:21:00 +0700

    last

--- model/model.php
+++ model/model.php
@@ -1,12 +1,12 @@
 <?php
 class model{
     protected $conn;
-    
+
     public function __construct(){
       $server         = "localhost";
       $username       = "root";
       $pass           = "";
-      $db             = "tubwebdas";
+      $db             = "b5_21137411_myhomestay";
       $this->conn = new mysqli($server, $username, $pass, $db);
     }
 }
